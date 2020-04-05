eaa9c68f6cbb0cecf4cc8eded10a87c1218ece80
Mai Houa Vue <mhvue4@gmail.com>
2020-04-05 13:32:25 -0500

    added basic buttons and layout ideas for html

---
+++ routes/apiRoutes.js
@@ -0,0 +1,1 @@
+//api.get and api.post routes here 
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-//need models required here 
+var db = require("./models"); 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,6 +18,10 @@ mongoose.connect("mongodb://localhost/scrapenewsdb", { useNewUrlParser: true });
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapenewsdb";
 
 mongoose.connect(MONGODB_URI);
+
+//routes here
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function(){
     console.log("Listening on " + PORT)
